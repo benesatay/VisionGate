593386b7e1e575d2750eb9dbfc15747a3f1f69dc
Bahadır Enes Atay <benesatayy@gmail.com>
2019-07-03 10:43:11 +0300

    Initial Commit

---
+++ visionGate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  visionGate
+//
+//  Created by Bahadır Enes Atay on 3.07.2019.
+//  Copyright © 2019 Bahadır Enes Atay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
